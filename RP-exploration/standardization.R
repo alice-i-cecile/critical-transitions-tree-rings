521cc7d2776d9fb9edee7d28830a43f6111b965e
alice.i.cecile <alice.i.cecile@gmail.com>
2014-07-08 13:41:46 -0400

    Created a merged TRA containing all data.

--- RP-exploration/standardization.R
+++ RP-exploration/standardization.R
@@ -7,12 +7,29 @@ library(ggplot2)
 # Configuration ####
 
 data_directory <- "./RP-exploration/data/RWL"
+tra_directory <- "./RP-exploration/data/TRA"
+
 seperate_residuals_directory <- "./RP-exploration/residuals/seperate"
 joint_residuals_directory <- "./RP-exploration/residuals/joint"
 
-
 # Loading in list of data sets
 file_names <- list.files(data_directory)
+
+# Reading and merging all chronologies ####
+rwl_list <- lapply(paste(data_directory, file_names, sep="/"), read.rwl)
+
+tra_list <- lapply(rwl_list, rwl_to_tra)
+
+site_id_list <- sapply(file_names, function(site){str_replace(site, pattern = "\\.rwl", replacement = "")})
+
+for (site_num in 1:length(site_id_list)){
+  tra_list[[site_num]]$Site <- site_id_list[site_num]
+}
+
+joint_tra <- Reduce(rbind, tra_list)
+joint_tra$Time_Split <- joint_tra$Site
+
+write.csv(joint_tra, file=paste(tra_directory, "Pr_joint.csv", sep="/"))
 
 # Standardizing each data set seperately ####
 
@@ -39,4 +56,9 @@ for (site in file_names){
   # Saving the residuals
   write.csv(std_output$dat$residuals, file=paste0(seperate_residuals_directory, "/", site_id, "_sep_resid.csv"))
 }
+
+# Standardizing the chronologies together ####
+
+standardize_tra(joint_tra, )
+
 
