264dbcd9937dbcfa95de2df51c0dcd5b00c4e560
alice.i.cecile <alice.i.cecile@gmail.com>
2014-07-07 20:19:27 -0400

    Cleaning and merging .rwl files.

---
+++ RP-exploration/rwl_merging.R
@@ -0,0 +1,32 @@
+# Libraries
+library(stringr)
+library(dplR)
+
+# Finding data
+data_directory <- "./red_pine_data/RWL_split"
+output_directory <- "./red_pine_data/RWL"
+
+
+file_names <- list.files("./red_pine_data/RWL_split")
+
+# Identifying site by file name
+site_names <- sapply(file_names, function(x){
+    rough_site <- str_extract(x, pattern=".*\\(")
+    site <- substr(rough_site, 1, nchar(rough_site)-1)
+    return(site)
+})
+
+site_list <- unique(site_names)
+
+# Repackaging files for each site
+
+for (site in site_list){
+  site_files <- file_names[site_names==site]
+  
+  series_list <- lapply(paste(data_directory, site_files, sep="/"), function(x){print(x);read.rwl(x)})
+  
+  series <- combine.rwl(series_list)
+  
+  # Saving packaged series as a single file
+  write.rwl(series, paste0(output_directory, "/", site, ".rwl"))
+}
