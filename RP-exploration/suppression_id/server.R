32841db1a07b715ecf1082752c1a6a5cc5d702f0
alice.i.cecile <alice.i.cecile@gmail.com>
2014-07-12 12:58:24 -0400

    Scaffolding for server.R, setting up all required variables.

--- RP-exploration/suppression_id/server.R
+++ RP-exploration/suppression_id/server.R
@@ -2,4 +2,46 @@
 library(shiny)
 
 shinyServer(function(input, output) {
+  
+  # Uploading data ####
+  {
+  input$series 
+  
+  input$old_suppression_data
+  }
+  
+  # Downloading results ####
+  {
+    output$suppression_data
+  }
+  
+  # Reactive UI for selecting series and years ####
+  {
+    output$series_list
+    
+    output$year_list
+  }
+  
+  # Flagging suppression and release events ####
+  {
+    input$flag_suppression
+    
+    input$flag_release
+    
+    input$clear_flags
+  }
+  
+  # Plot of series with flags ####
+  {
+    output$series_plot
+  }
+  
+  # Tables of suppression results #### 
+  {
+    output$series_flags
+    
+    output$suppression_data_table
+    
+    
+  }
 })
